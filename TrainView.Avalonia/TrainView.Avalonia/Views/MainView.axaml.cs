a61f2bc5c7b66ad5b0a91015c6ab48471c30bf4c
Erno Szabados <erno.szabados@windowslive.com>
2023-03-15 22:04:52 +0100

    Avalonia sample project.

---
+++ TrainView.Avalonia/TrainView.Avalonia/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace TrainView.Avalonia.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
